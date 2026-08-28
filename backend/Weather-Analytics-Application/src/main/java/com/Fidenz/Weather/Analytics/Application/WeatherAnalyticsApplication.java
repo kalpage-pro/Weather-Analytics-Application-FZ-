fcb71fdd4f85e8f5176a988548aa283c407b6aab
KALPAGE <minidu099@gmail.com>
2026-08-28 23:47:40 +0530

    spring initializr structure

---
+++ backend/Weather-Analytics-Application/src/main/java/com/Fidenz/Weather/Analytics/Application/WeatherAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.Fidenz.Weather.Analytics.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAnalyticsApplication.class, args);
+	}
+
+}
---
+++ backend/Weather-Analytics-Application/src/test/java/com/Fidenz/Weather/Analytics/Application/WeatherAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fidenz.Weather.Analytics.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
